1769c2fd1e38ddd146c4ee9635391cc53e7cc2d0
Tembai <60400554+Tembai@users.noreply.github.com>
2022-04-01 10:40:25 +0200

    Code werkt
    Alles werkt, programmer doet het niet maar heb een nieuwe. De module is nu nog iedere 10 minuten aan het zenden.

--- LoRaWAN/App/lora_app.h
+++ LoRaWAN/App/lora_app.h
@@ -49,7 +49,7 @@ extern "C" {
 /*!
  * Defines the application data transmission duty cycle. 10s, value in [ms].
  */
-#define APP_TX_DUTYCYCLE                            600000
+#define APP_TX_DUTYCYCLE                            5100000
 
 /*!
  * LoRaWAN User application port
