523a9ed63e4d890412c526c38d45bc6f98f7caf4
Tembai <60400554+Tembai@users.noreply.github.com>
2022-04-20 16:45:39 +0200

    Laatste update

--- Core/Inc/applicatie.c
+++ Core/Inc/applicatie.c
@@ -164,34 +164,6 @@ float Sensor_Data(void){
 
 
 
-void I2C_scan(void){
-	//https://deepbluembedded.com/stm32-i2c-scanner-hal-code-example/
-	  uint8_t Buffer[25] = {0};
-	  uint8_t Space[] = "-";
-	  uint8_t StartMSG[] = "Starting I2C Scanning: \r\n";
-	  uint8_t EndMSG[] = "Done! \r\n\r\n";
-	  uint8_t i = 0, ret;
-
-	  /*-[ I2C Bus Scanning ]-*/
-	      APP_LOG(TS_OFF, VLEVEL_M, StartMSG);
-	      for(i=1; i<128; i++)
-	      {
-	          ret = HAL_I2C_IsDeviceReady(&hi2c2, (uint16_t)(i<<1), 6, 25);
-	          if (ret != HAL_OK) /* No ACK Received At That Address */
-	          {
-	              APP_LOG(TS_OFF, VLEVEL_M, Space);
-	          }
-	          else if(ret == HAL_OK)
-	          {
-	              APP_LOG(TS_OFF, VLEVEL_M, Buffer,"\n");
-	          }
-              APP_LOG(TS_OFF, VLEVEL_M, Buffer,"\n");
-	      }
-	      APP_LOG(TS_OFF, VLEVEL_M, EndMSG);
-	      /*--[ Scanning Done ]--*/
-}
-
-
 void software_reset(void){
     HAL_StatusTypeDef ret;
     uint8_t var[1];
